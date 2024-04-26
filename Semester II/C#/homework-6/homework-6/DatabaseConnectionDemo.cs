03a3da528fa0ad3abf77f342f4a91dd2d27bdc51
Tornike Kikacheishvili <storage@Tornikes-MacBook-Pro.local>
2024-04-26 19:27:59 +0400

    c#: Homework

--- Semester II/C#/homework-6/homework-6/DatabaseConnectionDemo.cs
+++ Semester II/C#/homework-6/homework-6/DatabaseConnectionDemo.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 public class DatabaseConnectionDemo
 {
     
+    // შექმენით პროექტი, სადაც დაამუშავებთ მონაცემთა ბაზას Connected რეჟიმის კლასების გამოყენებით. შეასრულეთ რამდენიმე sql მოთხოვნა.
     public static void Run()
     {
         string connectionString = "Server=localhost,1433;Database=ImaginaryWorld;User=sa;Password=1234;";
@@ -13,6 +14,23 @@ public class DatabaseConnectionDemo
         InsertData(connectionString);
         UpdateData(connectionString);
         DeleteData(connectionString);
+
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+
+        Console.WriteLine("Enter new creature details:");
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        Console.Write("Type: ");
+        string type = Console.ReadLine();
+        Console.Write("Habitat: ");
+        string habitat = Console.ReadLine();
+        Console.Write("Age: ");
+        int age = int.Parse(Console.ReadLine());
+        Console.Write("Description: ");
+        string description = Console.ReadLine();
+
+        AddCreatureWithLocation(connectionString, name, type, habitat, age, description);
 
         Console.WriteLine("Press any key to exit...");
         Console.ReadKey();
@@ -82,4 +100,60 @@ public class DatabaseConnectionDemo
             Console.WriteLine($"Rows affected by delete: {rowsAffected}");
         }
     }
+    
+    static void AddCreatureWithLocation(string connectionString, string name, string type, string habitat, int age, string description)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlTransaction transaction = connection.BeginTransaction();
+
+                try
+                {
+                    string insertCreatureQuery = "INSERT INTO Creatures (Name, Type, Habitat, Age, Description) VALUES (@Name, @Type, @Habitat, @Age, @Description); SELECT SCOPE_IDENTITY();";
+                    SqlCommand insertCreatureCommand = new SqlCommand(insertCreatureQuery, connection, transaction);
+                    insertCreatureCommand.Parameters.AddWithValue("@Name", name);
+                    insertCreatureCommand.Parameters.AddWithValue("@Type", type);
+                    insertCreatureCommand.Parameters.AddWithValue("@Habitat", habitat);
+                    insertCreatureCommand.Parameters.AddWithValue("@Age", age);
+                    insertCreatureCommand.Parameters.AddWithValue("@Description", description);
+                    int creatureId = Convert.ToInt32(insertCreatureCommand.ExecuteScalar());
+
+                    Console.WriteLine("Enter location details:");
+                    Console.Write("Location Name: ");
+                    string locationName = Console.ReadLine();
+                    Console.Write("Terrain Type: ");
+                    string terrainType = Console.ReadLine();
+                    Console.Write("Climate: ");
+                    string climate = Console.ReadLine();
+                    Console.Write("Location Description: ");
+                    string locationDescription = Console.ReadLine();
+
+                    string insertLocationQuery = "INSERT INTO Locations (Name, TerrainType, Climate, Description) VALUES (@Name, @TerrainType, @Climate, @Description); SELECT SCOPE_IDENTITY();";
+                    SqlCommand insertLocationCommand = new SqlCommand(insertLocationQuery, connection, transaction);
+                    insertLocationCommand.Parameters.AddWithValue("@Name", locationName);
+                    insertLocationCommand.Parameters.AddWithValue("@TerrainType", terrainType);
+                    insertLocationCommand.Parameters.AddWithValue("@Climate", climate);
+                    insertLocationCommand.Parameters.AddWithValue("@Description", locationDescription);
+                    int locationId = Convert.ToInt32(insertLocationCommand.ExecuteScalar());
+
+                    string insertCreatureLocationQuery = "INSERT INTO CreatureLocations (CreatureID, LocationID) VALUES (@CreatureID, @LocationID)";
+                    SqlCommand insertCreatureLocationCommand = new SqlCommand(insertCreatureLocationQuery, connection, transaction);
+                    insertCreatureLocationCommand.Parameters.AddWithValue("@CreatureID", creatureId);
+                    insertCreatureLocationCommand.Parameters.AddWithValue("@LocationID", locationId);
+                    insertCreatureLocationCommand.ExecuteNonQuery();
+
+                    transaction.Commit();
+
+                    Console.WriteLine("Creature added with location.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error occurred. Rolling back transaction.");
+                    Console.WriteLine(ex.Message);
+                    transaction.Rollback();
+                }
+            }
+        }
 }
