c313d9090625d68a33f9529b6c78d581a4fc75b6
Tornike Kikacheishvili <storage@Tornikes-MacBook-Pro.local>
2024-06-07 15:22:53 +0400

    c# homework 10

--- Semester II/C#/homework-9/homework-9/Program.cs
+++ Semester II/C#/homework-9/homework-9/Program.cs
@@ -1,2 +1,4 @@
 ﻿// dotnet ef migrations add InitialCreate
 // dotnet ef database update
+
+// dotnet ef dbcontext scaffold "Server=localhost,1433;Database=ImaginaryWorld;User=sa;Password=1234;" Microsoft.EntityFrameworkCore.SqlServer -o Models
